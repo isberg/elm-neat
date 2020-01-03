398cda4fa45c1356e57de2f1f2b67817856bc14d
Jonas Isberg <jonas.d.isberg@gmail.com>
2020-01-03 11:29:27 +0100

    feat: create connected Genome

--- src/Genome.elm
+++ src/Genome.elm
@@ -1,11 +1,14 @@
-module Genome exposing (Genome, createUnconnected, toString)
+module Genome exposing (Genome, createUnconnected, createConnected, toString)
 
 import List
+import Random exposing (Generator)
+import List.Extra
 
 type Genome = Genome 
     { sensors : List Int
     , outputs : List Int
     , hidden : List Int
+    , connections : List (Int, Int)
     }
 
 createUnconnected : Int -> Int -> Genome
@@ -14,16 +17,38 @@ createUnconnected sensors outputs =
     { sensors = List.range 0 sensors
     , outputs = List.range (sensors + 1) (sensors + outputs)
     , hidden = []
+    , connections = []
     }
+
+createConnected : Int -> Int -> Float -> Int -> List a -> Generator (Genome, Int, List a)
+createConnected sensors outputs weightSpan id cache =
+    let
+        (Genome genome) = createUnconnected sensors outputs 
+        edges : List (Int, Int)
+        edges = 
+            List.Extra.lift2 (\from to -> (from, to)) genome.sensors genome.outputs
+            --|> List.indexedMap (\i (f, t) -> (i, f, t))
+    in
+    (Genome {genome | connections = edges}, id, cache) |> Random.constant
+
+empty : Genome
+empty = Genome {sensors = [], outputs = [], hidden = [], connections = []}
 
 toString : Genome -> String
 toString genome =
+    let
+        c2s = 
+            List.map (\(from, to) -> [s from, s to] |> format "{0}->{1}")
+            >> String.join ","
+    in
     case genome of
-        Genome {sensors, outputs, hidden} ->
-            [sensors, outputs, hidden] |> format "Genome {0} {1} {2}"
-     
+        Genome {sensors, outputs, hidden, connections} ->
+            [s sensors, s outputs, s hidden, c2s connections] |> format "Genome {0} {1} {2} [{3}]"
 
-format : String -> List a -> String
+s : a -> String
+s = Debug.toString
+
+format : String -> List String -> String
 format template values =
     let
         format_rec index vals templ =
@@ -31,7 +56,7 @@ format template values =
                 [] -> templ
                 head::tail -> 
                     templ 
-                    |> String.replace ("{" ++ (index |> String.fromInt) ++ "}") (head |> Debug.toString)
+                    |> String.replace ("{" ++ (index |> String.fromInt) ++ "}") head
                     |> format_rec (index + 1) tail
     in
     format_rec 0 values template 
--- tests/GenomeTests.elm
+++ tests/GenomeTests.elm
@@ -1,8 +1,9 @@
 module GenomeTests exposing (suite)
 
 import Expect
-import Test exposing (Test, describe, test)
 import Genome
+import Random
+import Test exposing (Test, describe, test)
 
 
 suite : Test
@@ -17,6 +18,27 @@ suite =
                     in
                     Genome.createUnconnected sensors outputs 
                     |> Genome.toString 
-                    |> Expect.equal "Genome [0,1,2] [3] []"
+                    |> Expect.equal "Genome [0,1,2] [3] [] []"
+            , test "can be created fully connected" <|
+                \_ -> 
+                    let 
+                        sensors = 2
+                        outputs = 1
+                        weightSpan = 6
+                        nextInnovation = 1
+                        innovationCache = [] 
+                        seed = Random.initialSeed 0
+
+                        sut = Genome.createConnected 
+                            sensors 
+                            outputs 
+                            weightSpan
+                            nextInnovation 
+                            innovationCache
+                        ((actual, _, _), _) = Random.step sut seed
+                    in
+                    actual
+                    |> Genome.toString
+                    |> Expect.equal "Genome [0,1,2] [3] [] [0->3,1->3,2->3]"
             ]
         ]
